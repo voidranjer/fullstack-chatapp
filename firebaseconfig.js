578d8f79af72cf1d85b06dae432685973e3683e3
meekranjer <logicism81@gmail.com>
2022-06-20 23:23:07 +0800

    Removed Firebase AppCheck
    Firebase AppCheck is not currently supported for Web Apps

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
+// import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,9 +18,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const db = getFirestore();
-const appCheck = initializeAppCheck(app, {
-  provider: new ReCaptchaV3Provider(process.env.RECAPTCHA),
-  isTokenAutoRefreshEnabled: true,
-});
+// const appCheck = initializeAppCheck(app, {
+//   provider: new ReCaptchaV3Provider(process.env.RECAPTCHA),
+//   isTokenAutoRefreshEnabled: true,
+// });
 
 export { auth, db };

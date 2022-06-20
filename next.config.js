b2916fd1044a304e708d2d4526048365076a88d0
meekranjer <logicism81@gmail.com>
2022-06-21 00:06:53 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    APP_ID: "1:9311945166:web:c1e3932fe27d228dcbeb9f",
+    MESSAGING_SENDER_ID: "9311945166",
+    STORAGE_BUCKET: "temp-chatapp.appspot.com",
+    PROJECT_ID: "temp-chatapp",
+    AUTH_DOMAIN: "temp-chatapp.firebaseapp.com",
+    API_KEY: "AIzaSyDTKhoWxsh9ZtJBL1a8UwCV9YSa3ChB3eI",
+  },
+};
